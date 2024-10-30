a08ac832443aadea38a97d53b9cce212408819c8
Watz <watztheengineer@gmail.com>
2024-10-30 15:13:56 -0400

    Refactoring

--- Source/TP_TrouNoir/Private/BlackHole.cpp
+++ Source/TP_TrouNoir/Private/BlackHole.cpp
@@ -1,18 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BlackHole.h"
 #include "Components/SphereComponent.h"
 #include "NiagaraComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "PhysicsEngine/RadialForceComponent.h"
+#include "TimerManager.h"
 
-
-// Sets default values
 ABlackHole::ABlackHole()
 {
 	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision Sphere"));
 	SetRootComponent(CollisionSphere);
+
 	BulletFX = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Effet"));
 	BulletFX->SetupAttachment(CollisionSphere);
 
@@ -24,12 +21,54 @@ ABlackHole::ABlackHole()
 
 	ProjectileComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile movements"));
 	ProjectileComponent->ProjectileGravityScale = 0;
+
+	IsExploding = false;
 }
 
-// Called when the game starts or when spawned
 void ABlackHole::BeginPlay()
 {
 	Super::BeginPlay();
-	RadialForceComponent->AttachToComponent(CollisionSphere,FAttachmentTransformRules::KeepRelativeTransform);
+	
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle_Explosion, this, &ABlackHole::Explode, 3.0f, false);
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle_Deletion, this, &ABlackHole::Delete, 10.0f, false);
+	
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &ABlackHole::OnOverlapBegin);
 }
 
+void ABlackHole::Explode()
+{
+	IsExploding = true;
+	ProjectileComponent->StopMovementImmediately();
+	BulletFX->Activate();
+	StaticMeshComponent->DestroyComponent();
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle_Force, this, &ABlackHole::Impulse, .1f, true);
+}
+
+void ABlackHole::Impulse()
+{
+	if (RadialForceComponent)
+	{
+		RadialForceComponent->FireImpulse();
+	}
+}
+
+void ABlackHole::Delete()
+{
+	Destroy();
+}
+
+bool ABlackHole::ShouldDeleteActor(const AActor* Actor) const
+{
+	return IsExploding &&
+		Actor != this &&
+		Actor->GetRootComponent()->Mobility == EComponentMobility::Movable &&
+		!Actor->IsA(APawn::StaticClass());
+}
+
+void ABlackHole::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ShouldDeleteActor(OtherActor))
+	{
+		OtherActor->Destroy();
+	}
+}
--- Source/TP_TrouNoir/Public/BlackHole.h
+++ Source/TP_TrouNoir/Public/BlackHole.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,6 +5,10 @@
 #include "BlackHole.generated.h"
 
 class USphereComponent;
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+class URadialForceComponent;
+class UStaticMeshComponent;
 
 UCLASS()
 class TP_TROUNOIR_API ABlackHole : public AActor
@@ -14,31 +16,42 @@ class TP_TROUNOIR_API ABlackHole : public AActor
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
+
 	ABlackHole();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
+	
+	void Explode();
+	void Impulse();
+	void Delete();
+	bool ShouldDeleteActor(const AActor* Actor) const;
+	
+	FTimerHandle TimerHandle_Explosion;
+	FTimerHandle TimerHandle_Deletion;
+	FTimerHandle TimerHandle_Force;
 
-	UPROPERTY(BlueprintReadWrite)
-	class UNiagaraComponent* BulletFX;
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	bool IsExploding;
 
-	UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(VisibleAnywhere)
 	USphereComponent* CollisionSphere;
 
 	UPROPERTY(BlueprintReadWrite)
-	class UProjectileMovementComponent* ProjectileComponent;
+	UNiagaraComponent* BulletFX;
+
+	UPROPERTY(BlueprintReadWrite)
+	UProjectileMovementComponent* ProjectileComponent;
 
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
-	class URadialForceComponent* RadialForceComponent;
+	URadialForceComponent* RadialForceComponent;
 
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (AllowPrivateAccess))
 	UStaticMeshComponent* StaticMeshComponent;
-	
+
 public:
 
-	UPROPERTY(BlueprintReadWrite,EditDefaultsOnly)
-	bool IsExploding;
-
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
